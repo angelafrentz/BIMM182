48047a4902f4bfcc9228033c5edf3c20a6044c0b
Angela Frentzen <angela@Angelas-MacBook-Air-7.local>
2017-05-30 16:35:33 -0700

    Updated Graoh and corrected negative controls

--- Operon_Dist_Analysis/density.py
+++ Operon_Dist_Analysis/density.py
@@ -39,7 +39,7 @@ for line in curs_operons:
 	for i in range(1, length):
 		firstRight = operonGeneInfo[i-1][2]
 		secondLeft = operonGeneInfo[i][1]
-		dist = secondLeft-firstRight
+		dist = secondLeft - firstRight +1
 		posCtrlDist.append(dist)
 
 #Creates a table of gene id's, bNumbers and left/right positions
@@ -57,28 +57,30 @@ operonsTable = curs_operon.fetchall()
 #Loops through operons to get the negative control distances
 i = 0
 count = 0
-while i < len(operonsTable)-1:
+while i < len(operonsTable):
 	#Accesses the operon names
-	prevOperon = operonsTable[i][0]
-	operon = operonsTable[i+1][0]
+	operon = operonsTable[i][0]
+	#operon = operonsTable[i+1][0]
 	#Only acesses opeorns that contain genes associated with a bNum
-	if (prevOperon in geneID) and (operon in geneID):
-		#Finds operons that are in the same 'directon', or rather on the same strand
-		if operonsTable[i][1] == operonsTable[i+1][1]:
-			#Gets the list of exon coordinates for the first operon
-			geneListPrev = geneID[prevOperon]
-			prevCommand = 'select g.locus_tag, e.left_pos, e.right_pos from genes g join exons e using(gene_id) where g.locus_tag in (\''+ '\',\''.join(geneListPrev) + '\')order by e.right_pos desc limit 1;'
-			curs_prev = db.cursor()
-			curs_prev.execute(prevCommand)
-			maxRightPrev = curs_prev.fetchall()[0][2]
-			#Gets a list of exon coordinates for the second operon
-			geneListCurr = geneID[operon]
-			command = 'select g.locus_tag, e.left_pos, e.right_pos from genes g join exons e using(gene_id) where g.locus_tag in (\''+ '\',\''.join(geneListCurr) + '\')order by e.left_pos asc limit 1;'
-			curs_curr = db.cursor()
-			curs_curr.execute(command)
-			minLeftCurr = curs_curr.fetchall()[0][1]
-			#Stores Negative Control Distances
-			dist = minLeftCurr - maxRightPrev
+	if (operon in geneID):
+		#Gets the list of exon coordinates for genes in the first operon
+		geneList = geneID[operon]
+		command = 'select g.locus_tag, e.left_pos, e.right_pos, g.strand from genes g join exons e using(gene_id) where g.locus_tag in (\''+ '\',\''.join(geneList) + '\') order by e.right_pos desc limit 1;'
+		curs_prev = db.cursor()
+		curs_prev.execute(command)
+		prev = curs_prev.fetchall()[0]
+		strandPrev = prev[3]
+		maxRight = prev[2]
+		#Finds the closest command
+		curs_closestGene = db.cursor()
+		command = 'select g.strand, e.left_pos from genes g join exons e using(gene_id) where e.left_pos > ' + str(maxRight) + ' order by left_pos asc limit 1;'
+		curs_closestGene.execute(command)
+		curr = curs_closestGene.fetchall()[0]
+		strandCurr = curr[0]
+		minLeft = curr[1]
+		#Stores Negative Control Distances
+		if strandPrev == strandCurr:
+			dist = minLeft - maxRight + 1
 			negCtrlDist.append(dist)
 	i += 1
 print "\nThe number of positive controls analyzed is:", len(posCtrlDist)
@@ -97,5 +99,9 @@ posCtrlDist = [ str(num) for num in posCtrlDist ]
 negCtrlDist = [str(num) for num in negCtrlDist ]
 posCtrl.write('\n'.join(posCtrlDist))
 negCtrl.write('\n'.join(negCtrlDist))
-
+#outputs a table of bNumbers
+operonDict = open('operonBNums.txt', 'w')
+for operon in geneID:
+	for gene in geneID[operon]:
+		operonDict.write( operon + '\t' + gene + '\n' )
 
